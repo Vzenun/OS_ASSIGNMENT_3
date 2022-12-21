ad0ff29cd918822d37546a06429b1d5ff102a58d
Vidur <vidur21364@iiitd.ac.in>
2022-12-21 22:32:57 +0530

    Commit

--- Ques3/q3.c
+++ Ques3/q3.c
@@ -4,23 +4,25 @@
 #include <linux/sched.h>
 #include <linux/cred.h>
 #include <linux/moduleparam.h>
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
-static int capture; 
-capture=0;
+//module_init(lkm_example_init);
+//module_exit(lkm_example_exit);
+static int capture=0;
 module_param(capture, int, 0644);
 static int __init lkm_example_init(void) {
 int pid;int leader;int uid;int comm;
 struct task_struct *thisprocess = pid_task(find_vpid(capture), PIDTYPE_PID);
 pid=thisprocess->pid;
 leader=thisprocess->group_leader->pid;
-uid=thisprocess->cred->uid;
-printk(KERN_INFO "Process uid: %d\n",uid);
-printk(KERN_INFO "Process pgid: %d\n",leader);
-printk(KERN_INFO "Process ID: %d\n", pid);
-printk(KERN_INFO "Process command path: %s\n",task->comm);
-return 4;
+printk(KERN_INFO "The pid of the process is: %d\n",pid);
+printk(KERN_INFO "The process group id is: %d\n",leader);
+printk(KERN_INFO "The process UID is: %d\n", thisprocess->cred->uid);
+printk(KERN_INFO "The command path of the process is: %s\n",thisprocess->comm);
+return 0;
 }
+//module_init(lkm_example_init);
+//module_exit(lkm_example_exit);
 static void __exit lkm_example_exit(void) {
 }
+module_init(lkm_example_init);
+module_exit(lkm_example_exit);
 MODULE_LICENSE(“GPL”);
